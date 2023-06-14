50b96cb076aa96098d15d92406470cc1352e838b
Yash <ya17kun@gmail.com>
2023-06-14 22:44:02 +0530

    add navbar & form component

---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,11 @@
+const AddEmployee = () => {
+  return (
+    <div className="flex max-w-2xl shadow border-b mx-auto">
+      <div className="px-8 py-8 font-thin text-2xl tracking-wider">
+        <div>Add New Employee</div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center">
+        <p className="text-white font-bold">Employee Portal</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
